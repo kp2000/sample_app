6860de14972238cf00aae436ab7fc5062ee255b3
kp2000 <kp2000@gmx.net>
2012-09-17 15:26:35 +0200

    fixing migrations

--- db/migrate/20120820184645_remove_avatars.rb
+++ db/migrate/20120820184645_remove_avatars.rb
@@ -1,7 +1,7 @@
 class RemoveAvatars < ActiveRecord::Migration
   def up
-  	remove_index :avatars,[:user_id]
-  	drop_table :avatars
+   remove_index :avatars,[:user_id]
+   drop_table :avatars
   end
 
   def down
